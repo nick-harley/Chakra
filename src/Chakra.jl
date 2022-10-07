f3bf88c4b9d9420aedc66e9ecc5b42fe3a3fe0bd
Nicholas Harley <nick.j.harley@gmail.com>
2022-10-07 08:33:49 +0200

    major refactor

--- src/Chakra.jl
+++ src/Chakra.jl
@@ -8,8 +8,6 @@ export Option
 export none, option_rec, omap, obind
 
 
-export Att, Prp
-
 export agg, pts, geta, seta, getp, setp, emp, ins, rmv, pop, peek, fnd, isemp, mem, dom, cts
 export seta!, setp!, ins!, rmv!, pop!
 export sequence
@@ -50,7 +48,7 @@ nil() = Any[]
 cons(h::T,t::List{T}) where T = T[h,t...]
 isnil(l::List) = isempty(l)
 
-function list_rec(pnil,pcons::Function,x::Vector) 
+function list_rec(pnil,pcons::Function,x::Vector) where T 
     function F(l)
         isnil(l) ? pnil : pcons(l[1],l[2:end],F(l[2:end]))
     end
@@ -93,123 +91,194 @@ end
 
 
 
-abstract type ATT{a,T} end
 
-abstract type PRP{p,T} end
 
-abstract type ID end
+abstract type Id end
 
-abstract type C end
+abstract type Constituent end
 
-abstract type H end
+abstract type Hierarchy end
+
+abstract type Attribute{a,T} end
+
+abstract type Property{p,T} end
+
+
+function __attributes__(::Val{n})::Attribute{n} where n
+    error("Name $n is not defined globally.")
+end
+__attributes__(n::Symbol) = __attributes__(Val{n}())
+__attributes__(n::String) = __attributes__(Symbol(n))
+
+
+function __properties__(::Val{n})::Property{n} where n
+    error("Name $n is not defined globally.")
+end
+__properties__(n::Symbol) = __properties__(Val{n}())
+__properties__(n::String) = __properties__(Symbol(n))
+
+
+
 
 # CONSTITUENT CONSTRUCTORS        
 
-function agg(ps::List{ID})::C
-    # agg : list ID -> C
-end 
-
-function seta(::ATT{a,T},
-              v::T,
-              c::C)::C where {a,T}
-    # forall a::A, typ a -> C -> C 
+function Error(name,TS...)
+    type_string = string(join(string.(typeof.(TS[1:end-1]))," -> ")," -> ",TS[end])
+    error(string("No method implementation of $name : ", type_string, ". \n $(methods(name))"))
 end
 
-function setp(::PRP{p,T},
+function agg(ps::List{T})::Constituent where {T<:Id}
+
+    # agg : list Id -> Constituent
+
+    Error(agg,ps,Constituent)
+
+end 
+
+function seta(a::Attribute{N,T},
               v::T,
-              c::C)::C where {p,T}
-    # forall p::P, T -> C -> C
+              c::Constituent)::Constituent where {N,T}
+
+    # forall a::A, typ a -> Constituent -> Constituent
+
+    Error(seta,a,v,c,Constituent)
+end
+
+function setp(p::Property{N,T},
+              v::T,
+              c::Constituent)::Constituent where {N,T}
+
+    # forall p::P, T -> Constituent -> Constituent
+
+    Error(setp,p,v,c,Constituent)
 end
 
 # CONSTITUENT DESTRUCTORS
 
-function geta(::ATT{a,T},
-              c::C)::Option{T} where {a,T}
-    # forall a::A, C -> option (typ a)
+function geta(a::Attribute{N,T},
+              c::Constituent)::Option{T} where {N,T}
+
+    # forall a::A, Constituent -> option (typ a)
+
+    Error(geta,a,c,Option{T})
 end
 
-function getp(::PRP{p,T},
-              c::C)::Option{T} where {p,T}
-    # forall p::P, C -> option (typ p)
+function getp(p::Property{N,T},
+              c::Constituent)::Option{T} where {N,T}
+
+    # forall p::P, Constituent -> option (typ p)
+
+    Error(getp,p,c,Option{T})
 end
 
-function pts(c::C)::List{ID}
-    # pts : C -> list ID
+function pts(c::Constituent)::List{Id}
+    
+    # pts : Constituent -> list Id
+
+    Error(pts,c,List{Id})
 end
 
 # HIERARCHY CONSTRUCTORS
 
-function emp(::Type{H})::H
+function emp(::Type{Hierarchy})::Hierarchy
+
     # emp : H
+
+    Error(emp,Hierarchy,Hierarchy)
 end
 
-function ins(x::ID,
-             c::C,
-             h::H)::H
-    # ins : ID -> C -> H -> H
+function ins(x::Id,
+             c::Constituent,
+             h::Hierarchy)::Hierarchy
+
+    # ins : Id -> Constituent -> Hierarchy -> Hierarchy
+
+    Error(ins,x,c,h,Hierarchy)
 end 
 
-function rmv(x::ID,
-             h::H)::H
-    # rmv : ID -> H -> H
+function rmv(x::Id,
+             h::Hierarchy)::Hierarchy
+
+    # rmv : Id -> H -> H
+
+    Error(rmv,x,h,Hierarchy)
 end
 
-function pop(h::H)::H
-    # pop : H -> H
+function pop(h::Hierarchy)::Hierarchy
+
+    # pop : Hierarchy -> Hierarchy
+
+    Error(pop,h,Hierarchy)
 end
 
 # HIERARCHY DESTRUCTORS
 
-function fnd(x::ID,
-             h::H)::Option{C}
-    # fnd : ID -> H -> option C
+function fnd(x::Id,
+             h::Hierarchy)::Option{Constituent}
+
+    # fnd : Id -> Hierarchy -> option Constituent
+
+    Error(x,h,Option{Constituent})
 end
 
-function peek(h::H)::Option{Pair{ID,C}}
-    # peek : H -> option (ID * C)
+function peek(h::Hierarchy)::Option{Pair{Id,Constituent}}
+
+    # peek : Hierarchy -> option (Id * Constituent)
+
+    Error(peek,h,Option{Pair{Id,Constituent}})
 end
 
-function cts(h::H)::List{Pair{ID,C}}
-    # cts : H -> list (ID * C)
+function cts(h::Hierarchy)::List{Pair{Id,Constituent}}
+
+    # cts : Hierarchy -> list (Id * C)
+
+    Error(cts,h,List{Pair{Id,Constituent}})
 end
 
-function dom(h::H)::List{ID}
-    # dom : H -> list ID
+function dom(h::Hierarchy)::List{Id}
+    
+    # dom : Hierarchy -> list Id
+
+    Error(dom,h,List{Id})
 end
 
 # BOOLEAN TESTS
 
-function isemp(h::H)::Bool
-    # isemp : H -> bool 
+function isemp(h::Hierarchy)::Bool
+    
+    # isemp : Hierarchy -> bool 
+
+    Error(isemp,h,Bool)
 end
 
-function mem(x::ID,
-             h::H)::Bool
-    # mem : ID -> H -> bool
+function mem(x::Id,
+             h::Hierarchy)::Bool
+    
+    # mem : Id -> Hierarchy -> bool
+
+    Error(mem,x,h,Bool)
 end
 
 # EFFECTFUL OPERATIONS
 
-function seta!(::ATT{a,T},
-               c::C)::C where {a,T} end
+function seta!(::Attribute{a,T},
+               c::Constituent)::Constituent where {a,T} end
 
-function setp!(::PRP{p,T},
-               c::C)::C where {p,T} end
+function setp!(::Property{p,T},
+               c::Constituent)::Constituent where {p,T} end
 
-function ins!(x::ID,
-              c::C,
-              h::H)::H end
+function ins!(x::Id,
+              c::Constituent,
+              h::Hierarchy)::Hierarchy end
 
-function rmv!(x::ID,
-              h::H)::H end
+function rmv!(x::Id,
+              h::Hierarchy)::Hierarchy end
 
-function pop!(h::H)::Option{Pair{ID,C}} end
-
-# DEPENDENT FAMILIES
+function pop!(h::Hierarchy)::Option{Pair{Id,Constituent}} end
 
 # ADDITIONAL OPERATIONS
 
-agg(ID::Type) = agg(ID[])
+agg(Id::Type) = agg(Id[])
 
 agg(m::Module) = agg(m.DOMAIN)
 
@@ -217,27 +286,36 @@ agg() = (println(@__MODULE__); agg(@__MODULE__))
 
 pts(x,h) = obind(fnd(x,h),c->pts(c))
 
-geta(a::Symbol,x,h) = obind(fnd(x,h),c->geta(a,c))
+geta(a::Symbol,c) = geta(__attributes__(a),c)
 
-getp(p::Symbol,x,h) = obing(fnd(x,h),c->getp(p,c))
+getp(p::Symbol,c) = getp(__properties__(p),c)
 
-#seta(a::Symbol,v,c) = seta(Att(a),v,c)
+geta(a::String,c) = geta(__attributes__(a),c)
 
-#seta!(a::Symbol,v,c) = seta!(Att(a),v,c)
+getp(p::String,c) = getp(__properties__(p),c)
+
+geta(a,x,h) = obind(fnd(x,h),c->geta(a,c))
+
+getp(p,x,h) = obing(fnd(x,h),c->getp(p,c))
+
+seta(a::Symbol,v,c) = seta(__attributes__(a),v,c)
+
+setp(p::Symbol,v,c) = setp(__properties__(p),v,c)
+
+seta(a::String,v,c) = seta(__attributes__(a),v,c)
+
+setp(p::String,v,c) = setp(__properties__(p),v,c)
 
 seta(a,v,x,h) = obind(fnd(x,h),c->seta(a,v,c))
 
-seta!(a,v,x,h) = obind(fnd(x,h),c->seta!(a,v,c))
-
-#setp(p::Symbol,v,c) = setp(Prp(p),v,c)
-
 setp(p,v,x,h) = obind(fnd(x,h),c->setp(p,v,c))
 
-#setp!(p::Symbol,v,c) = setp!(Prp(p),v,c)
+
+seta!(a,v,x,h) = obind(fnd(x,h),c->seta!(a,v,c))
 
 setp!(p,v,x,h) = obing(fnd(x,h),c->setp!(p,v,c))
 
-function sequence(xs::List{ID},h::H)::Option{List} where {ID,H}
+function sequence(xs::List{X},h::Hierarchy)::Option{List} where {X<:Id}
 
     # Dereference the list of ids to get their objects
     
@@ -247,7 +325,7 @@ function sequence(xs::List{ID},h::H)::Option{List} where {ID,H}
              xs)
 end
 
-function sequence(x::ID,h::H)::Option{List} where {ID,H}
+function sequence(x::Id,h::Hierarchy)::Option{List}
 
     # Dereference the particles of a constituent
 
@@ -257,121 +335,114 @@ end
 
 # REFERENCE IMPLEMENTATION
 
-macro Reference(ID,SUBS)
+macro Reference(Id,SUBS=[])
     
     esc(quote
-
-            using DataStructures
             
-            struct Id
-                value::($ID)
+            struct Id <: Chakra.Id
+                value::($Id)
             end
 
-            DOMAIN = Union{Id,[m.Id for m in $SUBS]...}
-
-            Ps = Dict{Symbol,Any}
-            As = Dict{Symbol,Any}
+            #submods = [eval(s) for s in $SUBS]                
+            ID_TYPES = Union{Id,[s.ID_TYPES for s in $SUBS]...}
             
-            struct Constituent
+            As = Dict{Symbol,Any}
+            Ps = Dict{Symbol,Any}
+            Parts = List{ID_TYPES}
+            
+            struct Constituent <: Chakra.Constituent
                 attributes::As
                 properties::Ps
-                particles::List{DOMAIN}
+                particles::Parts
             end
+
+            C_TYPES = Union{Constituent,[s.C_TYPES for s in $SUBS]...}
+
+            using DataStructures
 
             Cs = OrderedDict{Id,Constituent}
             
-            struct Hierarchy
+            struct Hierarchy <: Chakra.Hierarchy
                 constituents::Cs
             end
 
             mod = @__MODULE__
-            
-            struct AttName{a}
-                AttName(a::Symbol) = new{a}()
-            end
 
-            __atyp__(::AttName{a}) where a =
+            __attributes__(::Val{a}) where a =
                 error("Attribute $a is not defined in module $mod.")
 
-            __atyp__(a::Symbol)::DataType = __atyp__(AttName(a))
+            __attributes__(a::Symbol)::Type = __attributes__(Val{a}())
 
-            macro DefineAtt(a,T)    
-                esc(:(__atyp__(::AttName{$a}) = $T))
+            struct Attribute{a,T} <: Chakra.Attribute{a,T}
+                Attribute(a::Symbol) = new{a,__attributes__(a)}()
             end
 
-            struct Att{a,T}
-                a::Symbol
-                T::DataType
-                Att(a::Symbol) = begin
-                    T = __atyp__(a)
-                    new{a,T}(a,T)
-                end
+            macro DefineAttribute(a,T) 
+                esc(quote
+                        __attributes__(::Val{$a}) = $T
+                        g = Symbol(@__MODULE__,".",$a)
+                        Chakra.__attributes__(::Val{g}) = Attribute($a)
+                    end)
             end
 
-            struct PrpName{a}
-                PrpName(a::Symbol) = new{a}()
-            end
-
-            __ptyp__(::PrpName{p}) where p =
+            __properties__(::Val{p}) where p =
                 error("Property $p is not defined in module $mod.")
 
-            __ptyp__(p::Symbol)::DataType = __ptyp__(PrpName(p))
+            __properties__(p::Symbol)::Type = __properties__(Val{p}())
 
-            macro DefinePrp(p,T)    
-                esc(:(__ptyp__(::PrpName{$p}) = $T))
+            struct Property{p,T} <: Chakra.Property{p,T}
+                Property(p::Symbol) = new{p,__properties__(p)}()
             end
 
-            struct Prp{p,T}
-                p::Symbol
-                T::DataType
-                Prp(p::Symbol) = begin
-                    T = __ptyp__(p)
-                    new{p,T}(p,T)
-                end
+            macro DefineProperty(p,T)    
+                esc(quote
+                        __properties__(::Val{$p}) = $T
+                        g = Symbol(@__MODULE__,".",$p)
+                        Chakra.__properties__(::Val{g}) = Property($p)
+                    end)
             end
-
-
             
-            Chakra.agg(ps::List{DOMAIN}) =
+            Chakra.agg(ps::List{T}) where T<:ID_TYPES =
                 Constituent(As(),Ps(),ps)
 
-            Chakra.seta(::Att{a,T},
+            Chakra.agg(::Type{Constituent}) = agg(ID_TYPES[])
+
+            Chakra.seta(::Attribute{a,T},
                         v::T,
                         c::Constituent) where {a,T} =
                             Constituent(As(c.attributes...,a=>v),
                                         Ps(c.properties),
                                         c.particles)
 
-            Chakra.seta(a::Symbol,v,c) = Chakra.seta(Att(a),v,c)
+            Chakra.seta(a::Symbol,v,c) = Chakra.seta(Attribute(a),v,c)
             
-            Chakra.setp(::Prp{p,T},
+            Chakra.setp(::Property{p,T},
                         v::T,
                         c::Constituent) where {p,T} =
                             Constituent(As(c.attributes),
                                         Ps(c.properties...,p=>v),
                                         c.particles)
 
-            Chakra.setp(p::Symbol,v,c) =
-                Chakra.setp(Prp(p),v,c)
+            Chakra.setp(p::Symbol,v,c) = Chakra.setp(Property(p),v,c)
             
-            Chakra.pts(c::Constituent)::List{DOMAIN} =
+            Chakra.pts(c::Constituent)::List{ID_TYPES} =
                 c.particles
 
-            Chakra.geta(::Att{a,T},
+            Chakra.geta(::Attribute{a,T},
                         c::Constituent) where {a,T} =
                             Base.get(c.attributes,a,none)
 
             Chakra.geta(a::Symbol,
                         c::Constituent) =
-                            Chakra.geta(Att(a),c)
+                            Chakra.geta(Attribute(a),c)
             
-            Chakra.getp(::Prp{p,T},
+            Chakra.getp(::Property{p,T},
                         c::Constituent) where {p,T} =
                             Base.get(c.properties,p,none)
 
             Chakra.getp(p::Symbol,
                         c::Constituent) =
-                            Chakra.getp(Prp(p),c)
+                            Chakra.getp(Property(p),c)
             
             Chakra.emp(::Type{Hierarchy})::Hierarchy =
                 Hierarchy(Cs())
@@ -391,33 +462,41 @@ macro Reference(ID,SUBS)
             Chakra.fnd(x::Id,
                        h::Hierarchy)::Option{Constituent} =
                            Base.get(h.constituents,x,none)
+
+            for S in $SUBS
+                eval(:(Chakra.fnd(x::$S.Id,h::Hierarchy)::$S.Constituent = fnd(x,$S.data)))
+            end
             
-            Chakra.peek(h::Hierarchy)::Option{Pair{Id,Constituent}} =
-                isempty(h.constituents) ? none : collect(h.constituents)[end]
+            Chakra.peek(h::Hierarchy)::Option{Pair{ID_TYPES,C_TYPES}} =
+                cts(h)[1]
 
             Chakra.isemp(h::Hierarchy)::Bool =
-                isempty(h.constituents)
+                isempty(dom(h))
 
             Chakra.mem(x::Id,h::Hierarchy)::Bool =
                 haskey(h.constituents,x)
 
-            Chakra.cts(h::Hierarchy)::List{Pair{Id,Constituent}} =
-                reverse(collect(h.constituents))
+            for S in $SUBS
+                eval(:(Chakra.mem(x::$S.Id,h::Hierarchy)::Bool = mem(x,$S.data)))
+            end
+            
+            Chakra.cts(h::Hierarchy)::List{Pair{ID_TYPES,C_TYPES}} =
+                vcat(reverse(collect(h.constituents)),[cts(S.data) for S in reverse($SUBS)]...)
 
-            Chakra.dom(h::Hierarchy)::List{Id} =
-                reverse(collect(keys(h.constituents)))
+            Chakra.dom(h::Hierarchy)::List{ID_TYPES} =
+                vcat(reverse(collect(keys(h.constituents))),[dom(S.data) for S in reverse($SUBS)]...)
 
             Chakra.ins!(x::Id,
                         c::Constituent,
                         h::Hierarchy) =
                             h.constituents[x] = c
 
-            Chakra.seta!(::Att{a,T},
+            Chakra.seta!(::Attribute{a,T},
                          v::T,
                          c::Constituent) where {a,T} =
                              c.attribtues[a] = v
 
-            Chakra.setp!(::Prp{p,T},
+            Chakra.setp!(::Property{p,T},
                          v::T,
                          c::Constituent) where {p,T} =
                              c.properties[p] = v
@@ -435,7 +514,6 @@ end
 
 
 
-
 # VIEWPOINTS
 
 abstract type Viewpoint{T} end
@@ -443,9 +521,11 @@ abstract type Viewpoint{T} end
 returntype(v::Viewpoint{T}) where T = T
 
 struct AtomicViewpoint{T} <: Viewpoint{T}
-    attribute::Symbol
-    returntypes::List{DataType}
-    AtomicViewpoint(a::Symbol) = new{__typ__(a)}(a,[__typ__(a)])
+    attribute::Attribute
+    returntypes::List{Type}
+    AtomicViewpoint(a::Attribute{N,T}) where {N,T} = new{T}(a,[T])
+    AtomicViewpoint(a::Symbol) = AtomicViewpoint(__attributes__(a))
+    AtomicViewpoint(a::String) = AtomicViewpoint(__attributes__(a))
 end
 
 function (v::AtomicViewpoint{T})(s::List)::Option{T} where T 
