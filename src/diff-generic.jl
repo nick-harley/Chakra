85055660fd5eb1ee3f1cc4842c606bcf06ab0c60
Nicholas Harley <29707986+nick-harley@users.noreply.github.com>
2024-06-18 18:06:06 +0200

    removed ".value" from generic_diff implementation

--- src/diff-generic.jl
+++ src/diff-generic.jl
@@ -4,9 +4,9 @@ Chakra.diff(v::Viewpoint{T}, op::String) where T = apply_diff(v::Viewpoint{T}, o
 
 function apply_diff(v::Viewpoint{T}, op::String) where T
     if op == "Ratio"
-        return compose(link(v,delay(v,1)),(x,y)->y.value/x.value)
+        return compose(link(v,delay(v,1)),(x,y)->y/)
     elseif op == "Linear"
-        return compose(link(v,delay(v,1)),(x,y)->x.value-y.value)
+        return compose(link(v,delay(v,1)),(x,y)->x-y)
     else
         println("Error: Operator name is not available. You can take a look to the list of available operators.")
         println("Helper")
